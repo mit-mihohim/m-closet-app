a56d345b6715bf38a69fbde41c28be216ad16d30
mit-mihohim <mihohim.jp@gmail.com>
2020-06-15 21:37:45 +0900

    Basic認証追加
    本番環境のみ、id入力必須

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:username] &&
+      password == Rails.application.credentials[:basic_auth][:password]
+    end
+  end
 end

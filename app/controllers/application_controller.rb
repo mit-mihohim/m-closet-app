81c019048d09083bbebee82f3261da1a32f6b098
mit-mihohim <mihohim.jp@gmail.com>
2020-06-15 23:21:19 +0900

    環境変数呼び出しに変更
    本番環境にて、環境変数利用する為

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:username] &&
-      password == Rails.application.credentials[:basic_auth][:password]
+      username == ENV['BASIC_AUTH_USERNAME'] &&
+      password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end

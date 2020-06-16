53c8169ba30a5281b39e6ab3b7432ab6074ad15c
mit-mihohim <mihohim.jp@gmail.com>
2020-06-16 16:20:28 +0900

    usersマイグレーションファイル編集

--- db/migrate/20200616071057_devise_create_users.rb
+++ db/migrate/20200616071057_devise_create_users.rb
@@ -4,9 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
+      t.string :nickname,           null: false 
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at

3929f4fd72119afe61ec568770c20d5eb2c1f21e
mit-mihohim <mihohim.jp@gmail.com>
2020-06-16 12:27:06 +0900

    remove

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   protect_from_forgery with: :exception
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" &&
-      password == "4321"
-    end
-  end
 end

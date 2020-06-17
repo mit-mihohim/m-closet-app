1f950e6f07cf7b66f15f1a1feebabf701bdf7d62
mit-mihohim <mihohim.jp@gmail.com>
2020-06-17 22:27:33 +0900

    itemsテーブル作成

---
+++ db/migrate/20200617125813_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string        :image,    null: false
+      t.integer       :season,   null: false, default: 0
+      t.integer       :color,    null: false, default: 0
+      t.references    :category, null: false, foreign_key: true
+      t.references    :user,     null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_124705) do
+ActiveRecord::Schema.define(version: 2020_06_17_125813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 2020_06_17_124705) do
     t.index ["name"], name: "index_categories_on_name"
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "image", null: false
+    t.integer "season", default: 0, null: false
+    t.integer "color", default: 0, null: false
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", null: false
@@ -57,4 +69,6 @@ ActiveRecord::Schema.define(version: 2020_06_17_124705) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end

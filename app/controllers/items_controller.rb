e5735d3a7aae105a193a2c75aeefa7deec724456
mit-mihohim <mihohim.jp@gmail.com>
2020-06-18 12:53:59 +0900

    ストロングパラメーター追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :season, :color, :text, :category_id, :user_id)
+  end
 end

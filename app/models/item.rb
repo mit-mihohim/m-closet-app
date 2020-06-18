2bb665fce8c774882513850ebcf23d9b7e61e040
mit-mihohim <mihohim.jp@gmail.com>
2020-06-18 17:25:28 +0900

    item登録項目enum指定
    - season
    - color
    enum設定し、選択式にする

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  enum season: {"春夏": 1, "秋冬": 2, "その他": 3}
+  enum color: {"白": 1, "黒": 2, "赤": 3, "ピンク": 4, "黄": 5, "緑": 6, "青": 7, "紫": 8, "その他(柄)": 9}
 end

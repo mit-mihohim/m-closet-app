73f992ee45b731b0a4f618822296d0a52afc15df
mit-mihohim <mihohim.jp@gmail.com>
2020-06-18 18:39:57 +0900

    カテゴリーリレーション追記
    - enum カラー選択項目追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   has_one_attached :image
   enum season: {"春夏": 1, "秋冬": 2, "その他": 3}
-  enum color: {"白": 1, "黒": 2, "赤": 3, "ピンク": 4, "黄": 5, "緑": 6, "青": 7, "紫": 8, "その他(柄)": 9}
+  enum color: {"白": 1, "黒": 2, "ベージュ": 3, "紺": 4, "赤": 5, "ピンク": 6, "黄": 7, "緑": 8, "青": 9, "紫": 10, "その他(柄)": 11}
 end

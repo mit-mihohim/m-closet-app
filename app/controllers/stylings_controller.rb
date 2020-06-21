7173b85469826ece2609d07a2af886113ab791b6
mit-mihohim <mihohim.jp@gmail.com>
2020-06-21 23:26:50 +0900

    set_styling定義

--- app/controllers/stylings_controller.rb
+++ app/controllers/stylings_controller.rb
@@ -1,10 +1,11 @@
 class StylingsController < ApplicationController
+  before_action :items_index, only: [:new, :edit]
+  before_action :set_styling, only: [:show]
   def index
 
   end
 
   def show
-    
   end
 
   def new
@@ -33,6 +34,10 @@ class StylingsController < ApplicationController
   end
 
   private
+
+  def set_styling
+    @styling = Styling.find(params[:id])
+  end
 
   def styling_params
     params.require(:styling).permit(:title, :season, :scene, :text, item_ids: []).merge(user_id: current_user.id)

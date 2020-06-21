c57260ffa9ee606effbae928ccf2a3cf210e9019
mit-mihohim <mihohim.jp@gmail.com>
2020-06-21 11:19:26 +0900

    stylingモデル、バリデーション追記

--- app/models/styling.rb
+++ app/models/styling.rb
@@ -2,4 +2,6 @@ class Styling < ApplicationRecord
   belongs_to :user
   has_many :item_stylings
   has_many :items, through: :item_stylings
+
+  validates :title, :season, :scene, presence: true
 end

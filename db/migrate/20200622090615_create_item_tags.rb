97e370731c0c64b6aa26fbef2d6a3b733727b3a9
mit-mihohim <mihohim.jp@gmail.com>
2020-06-30 22:00:56 +0900

    マイグレーションファイル修正

--- db/migrate/20200622090615_create_item_tags.rb
+++ db/migrate/20200622090615_create_item_tags.rb
@@ -3,8 +3,6 @@ class CreateItemTags < ActiveRecord::Migration[6.0]
     create_table :item_tags do |t|
       t.references :item, null: false, foreign_key: true
       t.references :tag, null: false, foreign_key: true
-
-      t.timestamps
     end
   end
 end

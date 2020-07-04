5177a0f1bf424e06084384290b3e36ddff6e85c6
mit-mihohim <mihohim.jp@gmail.com>
2020-07-04 18:40:31 +0900

    Update application.js
    - stylesheet読み込みを追記

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-
+import "stylesheets/application.scss"
 import "layouts/up-btn.js";
 
 // Uncomment to copy all static images under ../images to the output folder and reference

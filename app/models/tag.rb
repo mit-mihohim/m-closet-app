e9fd857383a117eb48479744725821367f553713
mit-mihohim <mihohim.jp@gmail.com>
2020-07-05 16:38:04 +0900

    validation追加
    - ユーザーが独自のタグを作成できる
    - 一意性制約。
    - 既存の名前でない場合のみ登録可能
    - 大文字小文字は区別しない

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   has_many :item_tags, dependent: :destroy
   has_many :tags, through: :item_tags, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, case_sensitive: false
 end

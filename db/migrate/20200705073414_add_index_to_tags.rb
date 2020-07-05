b686bf7d34b0e28c163851ac5a9ca0e4dc07f8b9
mit-mihohim <mihohim.jp@gmail.com>
2020-07-05 16:38:41 +0900

    tagsにindex追加
    - tags-tableにindexを追加

---
+++ db/migrate/20200705073414_add_index_to_tags.rb
@@ -0,0 +1,5 @@
+class AddIndexToTags < ActiveRecord::Migration[6.0]
+  def change
+    add_index :tags, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_025406) do
+ActiveRecord::Schema.define(version: 2020_07_05_073414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_06_23_025406) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name"
   end
 
   create_table "users", force: :cascade do |t|

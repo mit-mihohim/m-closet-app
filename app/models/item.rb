7f11d283521d5afe9e8f30909d291beada537711
mit-mihohim <mihohim.jp@gmail.com>
2020-07-06 15:47:04 +0900

    Update item.rb
    - validation変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :favourite, through: :favourites, source: :user
 
-  validates :image, :tags, :season, :color, presence: true
+  validates :image, :tag_list, :season, :color, presence: true
 
   enum season: {"春夏": 1, "秋冬": 2, "その他": 3}
   enum color: {"白": 1, "黒": 2, "ベージュ": 3, "紺": 4, "赤": 5, "ピンク": 6, "黄": 7, "緑": 8, "青": 9, "紫": 10, "その他(柄)": 11}

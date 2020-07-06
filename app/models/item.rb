ba0c6769cfa60077d034327dad25b07821160645
mit-mihohim <mihohim.jp@gmail.com>
2020-07-06 15:22:10 +0900

    Update item.rb
    - acts_as_taggable :tagの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image, dependent: :destroy
-  has_many :item_tags, dependent: :destroy
-  has_many :tags, through: :item_tags, dependent: :destroy
+  acts_as_taggable_on :tags
+  acts_as_taggable_on :categories, :features
   has_many :favourites, dependent: :destroy
   has_many :favourite, through: :favourites, source: :user
 

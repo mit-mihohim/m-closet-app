116da2f46d13d98189cb21d3904d691bb6629513
mit-mihohim <mihohim.jp@gmail.com>
2020-07-06 18:05:17 +0900

    indexアクション追加
    - json形式で変換されるよう追記

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,9 @@
 class TagsController < ApplicationController
-  
+  def index
+    @tags = ActsAsTaggableOn::Tag.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 end

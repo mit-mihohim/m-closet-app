9f08adf1fd4d2525d29e69d673de3ae9fe8560a6
mit-mihohim <mihohim.jp@gmail.com>
2020-07-07 18:25:23 +0900

    Update tags.js
    - タグ検索結果表示

--- app/javascript/src/tags.js
+++ app/javascript/src/tags.js
@@ -1,7 +1,14 @@
 $(function() {
+  function addTag(tag) {
+    let html = `
+                <div class="tags-field">
+                  <div class="tag-search-add" data-tag-id="${tag.id}" data-tag-name="${tag.name}">${tag.name}</div>
+                </div>
+                `;
+                $("#tags-search-result").append(html);
+  }
   $("#item_tag_list").on("keyup", function () {
     let input = $("#item_tag_list").val();
-    console.log(input);
     $.ajax({
       type: "GET",
       url: "/tags",
@@ -9,6 +16,7 @@ $(function() {
       dataType: "json"
     })
     .done(function(tags) {
+      $("#tags-search-result").empty();
       if (tags.length !== 0) {
         tags.forEach(function(tag) {
           addTag(tag);

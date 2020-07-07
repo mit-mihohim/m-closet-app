0ff2662d1bb0f6906c367f7481cbfab44963bdc9
mit-mihohim <mihohim.jp@gmail.com>
2020-07-07 15:41:57 +0900

    値を非同期で送信

--- app/javascript/src/tags.js
+++ app/javascript/src/tags.js
@@ -2,5 +2,11 @@ $(function() {
   $("#item_tag_list").on("keyup", function () {
     let input = $("#item_tag_list").val();
     console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/tags",
+      data: {keyword: input},
+      dataType: "json"
+    })
   });
 });

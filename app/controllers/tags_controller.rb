fd9c5cb322550c9913704f8312d94406ead6f897
mit-mihohim <mihohim.jp@gmail.com>
2020-07-09 09:13:02 +0900

    Update tags_controller.rb
    - 検索結果を5件に指定

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def index
     @tags_all = ActsAsTaggableOn::Tag.all
-    @tags = ActsAsTaggableOn::Tag.named_like(params[:keyword])
+    @tags = ActsAsTaggableOn::Tag.named_like(params[:keyword]).limit(5)
     respond_to do |format|
       format.html
       format.json

0977e47a704eb33c4a2f44dafed32c757933d0ee
mit-mihohim <mihohim.jp@gmail.com>
2020-07-09 16:16:26 +0900

    既存タグ選択
    - 既存のタグを表示
    - 非同期での検索追加削除

--- app/javascript/src/tags.js
+++ app/javascript/src/tags.js
@@ -2,10 +2,35 @@ $(function() {
   function addTag(tag) {
     let html = `
                 <div class="tags-field">
-                  <div class="tag-search-add" data-tag-id="${tag.id}" data-tag-name="${tag.name}">${tag.name}</div>
+                  <p class="tags-field__name">${tag.name}</p>
+                  <div class="tag-search-add tags-field__btn tags-field__btn--add" data-tag-id="${tag.id}" data-tag-name="${tag.name}">Add</div>
                 </div>
                 `;
                 $("#tags-search-result").append(html);
+  }
+
+  function addNoTag() {
+    let html = `
+                <div class="tags-field">
+                  <p class="tags-field__name">該当タグはありません。上記から追加して下さい</p>
+                </div>
+                `;
+                $("#tags-search-result").append(html);
+  }
+
+  function addDeleteTag(name, id) {
+    let html = `
+          <div class="tags-field"id="${id}">
+            <p class="tags-field__name">${name}</p>
+            <div class="tag-search-remove tags-field__btn tags-field__btn--remove js-remove-btn" data-tag-id="${id}" data-tag-name="${name}">Delete</div>
+          </div>
+          `;
+          $("#tags-selected").append(html);
+  }
+
+  function addSelectedTags(tagId) {
+    let html = `<input value="${tagId}" name="item[tag_list]" type= "hidden", id="item_tag_list${tagId}" />`;
+    $(`#${tagId}`).append(html);
   }
   $("#item_tag_list").on("keyup", function () {
     let input = $("#item_tag_list").val();
@@ -21,11 +46,28 @@ $(function() {
         tags.forEach(function(tag) {
           addTag(tag);
         });
+      } else if (input.length == 0) {
+        return false;
       } else {
-        addCreateTag();
+        addNoTag();
       }
     })
     .fail(function() {
-    })
+      alert("通信エラーです。ページをリロードして下さい");
+    });
+  });
+  $(document).on("click", ".tags-field__btn--add", function() {
+    const tagName = $(this).attr("data-tag-name");
+    const tagId = $(this).attr("data-tag-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteTag(tagName, tagId);
+    addSelectedTags(tagId);
+  });
+  $(document).on("click", ".tags-field__btn--remove", function () {
+    $(this)
+      .parent()
+      .remove();
   });
 });

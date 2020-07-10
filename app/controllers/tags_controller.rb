91856056d065ae6e373d4c4a3c5b3ffe06ca5faa
mit-mihohim <mihohim.jp@gmail.com>
2020-07-10 15:08:18 +0900

    searchアクション追加
    - tags searach追加
    - ルーティング設定

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,9 @@
 class TagsController < ApplicationController
   def index
     @tags_all = ActsAsTaggableOn::Tag.all
+  end
+
+  def search
     @tags = ActsAsTaggableOn::Tag.named_like(params[:keyword]).limit(5)
     respond_to do |format|
       format.html
